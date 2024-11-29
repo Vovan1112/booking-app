3da3e1b7006acc4c04e2e8f372ff1c688a0ec3c7
Volodymyr <volodymyrbabinchuk@coherentsolutions.com>
2024-11-29 15:45:28 +0200

    Header-Block

---
+++ components/Header/HeaderLoginButton.tsx
@@ -0,0 +1,5 @@
+export const HeaderLoginButton = () => (
+    <button className="bg-blue-400 text-white font-bold py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300 transition-all">
+        Login
+    </button>
+);
---
+++ components/Header/HeaderLogo.tsx
@@ -0,0 +1,3 @@
+import Image from "next/image"
+
+export const HeaderLogo = () =>  <Image src="/HeaderLogo.png"  alt="Header Logo" width={150} height={150} />
